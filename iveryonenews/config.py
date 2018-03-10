bca1412ee718ed56d504efd248abe4fde3041f8d
panenming <panenming@enn.cn>
2018-03-10 10:09:55 +0800

    修改参数

--- iveryonenews/config.py
+++ iveryonenews/config.py
@@ -4,7 +4,7 @@ UID = "1714420"#用户uid
 HEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0"
 }#请求头部信息
-PAGECOUNT=2 #拉取多少页数据
+PAGECOUNT=1 #拉取PAGECOUNT + 1页数据
 JSONP_START= r'callback('
 JSONP_END = r');'
 JSONP="callback"
