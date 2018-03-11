bc94fae8763e5f88539f2a8de0bcc3a2f8f2d0df
panenming <panenming@enn.cn>
2018-03-11 19:58:04 +0800

    try catch

--- iveryonenews/freshAd.py
+++ iveryonenews/freshAd.py
@@ -7,16 +7,24 @@ import time
 # 刷新广告，赚取积分
 def freshAd():
     url = "https://userapi.iveryone.wuyan.cn/Api/Shadow/Query?uid=%s&token=%s&lang=zh-CN&limit=3"%(config.UID,config.TOKEN)
-    res = requests.get(headers=config.HEADERS,url=url)
-    if res.ok:
-        if res.json().get(u"errno") == 0:
-            print("刷新成功！")
-            return True
+    try:
+        res = requests.get(headers=config.HEADERS,url=url)
+        if res.ok:
+            if res.json().get(u"errno") == 0:
+                if len(res.json().get(u"data")) > 0:
+                    print("刷新成功！")
+                else:
+                    print("没有更多广告！")
+                return True
+            else:
+                print(res.text)
+                return False
         else:
-            print(res.text)
             return False
-    else:
+    except Exception:
+        print("刷新失败！")
         return False
+    
 
 if __name__ == '__main__':
     while True:
